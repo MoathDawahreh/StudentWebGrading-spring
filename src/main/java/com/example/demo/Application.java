e2f0b1f8317fd77a9013c837ed46cca24dde7cb1
MoathDawahreh <moath.dawahreh@gmail.com>
2022-01-14 23:06:25 +0200

    ci/docker

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -2,13 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-@ComponentScan({"com.example.demo.controllers"})
-@ComponentScan({"com.example.demo.data"})
-
 public class Application {
 
 	public static void main(String[] args) {
